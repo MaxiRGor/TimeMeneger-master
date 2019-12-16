ba0d4252c914f4766aab8663b3ac4c451be1b08a
MaxiRGor <maxirgor@gmail.com>
2019-12-16 12:47:34 +0300

    maxirgor initial commit: app moved to android X and dependencies updated

--- app/src/main/java/com/example/myapplication/DBReports.java
+++ app/src/main/java/com/example/myapplication/DBReports.java
@@ -3,7 +3,7 @@ package com.example.myapplication;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 
 public class DBReports extends SQLiteOpenHelper {
 
